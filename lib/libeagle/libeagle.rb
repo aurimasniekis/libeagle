9ccc554401e2bb3870ed12305570307a929e7be8
Aurimas Niekis <gcdsxs@gmail.com>
2012-11-18 01:02:21 +0200

    Added a little bit for testing

--- lib/libeagle/libeagle.rb
+++ lib/libeagle/libeagle.rb
@@ -1,12 +1,17 @@
 require_relative "eagle"
+require 'yaml'
 raise "LibEagle requires Ruby >= 1.8.6" if RUBY_VERSION < "1.8.6"
 
 module LibEagle
-
-  def parse(file)
-    xml = Nokogiri::XML(IO.read(file))
-    root = xml.xpath('//eagle').first
-    eagle = Eagle.new(root)
+  class << self
+    def parse(file)
+      xml = Nokogiri::XML(IO.read(file))
+      root = xml.xpath('//eagle').first
+      eagle = Eagle.new(root)
+      return eagle
+    end
   end
   
 end
+
+puts LibEagle::parse('/Users/gcds/Projects/Ruby/test.xml').to_yaml
