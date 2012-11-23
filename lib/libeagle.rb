e54549f0bf6959322f690c9182940532fbf7e32e
Aurimas Niekis <gcdsxs@gmail.com>
2012-11-23 11:16:39 +0200

    Writting main gem wrapper

--- lib/libeagle.rb
+++ lib/libeagle.rb
@@ -1,2 +1,12 @@
 require 'nokogiri'
-require_relative "libeagle"
+require 'pp'
+require 'yaml'
+require 'nokogiri'
+require 'HTMLEntities'
+require_relative "eagle/xml"
+require_relative "eagle/base"
+require_relative "eagle/eagle"
+require_relative "eagle/types"
+require_relative "eagle/libeagle"
+
+raise "LibEagle requires Ruby >= 1.8.6" if RUBY_VERSION < "1.8.6"
--- lib/libeagle/libeagle.rb
+++ lib/libeagle/libeagle.rb
@@ -1,15 +1,11 @@
-require_relative "xml"
-require_relative "base"
-require_relative "eagle"
-require_relative "types"
-require 'pp'
-require 'yaml'
-require 'nokogiri'
-require 'HTMLEntities'
-raise "LibEagle requires Ruby >= 1.8.6" if RUBY_VERSION < "1.8.6"
-
 module LibEagle
   class Parser
+
+  	def self.parseFile(file)
+  		content = IO.read(file)
+  		parseXML(content)
+  	end
+
     def self.parseXML(xml_content)
       xml = Nokogiri::XML(xml_content)
       root = xml.xpath('//eagle').first
@@ -17,8 +13,4 @@ module LibEagle
       return eagle
     end
   end
-  
-end
-
-xml = LibEagle::Parser.parseXML(IO.read('./schematic.sch'))
-puts xml.saveXML
+ 
